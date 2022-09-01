b2b96762c45dd00d120e05534edf051c209a4750
dayan <karoldayanvilla@gmail.com>
2022-09-01 14:35:54 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nombre = prompt('What is your name?');
+document.querySelector('.contenido').innerHTML = `${nombre} you are learning js moderno`;
+
+
+console.time('hello');
+
+
+console.warn('esto no esta permitido');
